e07668ac8e10c30c7235d5da6047a70773fb67e4
ReglandOnyemeka <135351219+ReglandOnyemeka@users.noreply.github.com>
2026-06-09 14:56:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // 1. AI API ROUTE
 app.post('/api/ai-assist', async (req, res) => {
-    const GEMINI_KEY = process.env.AI_API_KEY;
+    const GEMINI_KEY = process.env.AIzaSyBXUzwNGDO7XFrZPvBJVFYxw1tg30rqQNU;
     if (!GEMINI_KEY) return res.status(500).json({ error: "API Key missing in Render settings." });
 
     try {
